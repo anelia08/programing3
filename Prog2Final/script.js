48c85b8489a1ac2aba24278c8e784d28f7bb11f9
Your Name <you@example.com>
2022-02-08 16:16:16 +0400

    text

---
+++ Prog2Final/script.js
@@ -0,0 +1,144 @@
+function generator(matLen, gr, grEat, pred, flo, regen) {
+    let matrix = [];
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 1;
+        }
+    }
+    for (let i = 0; i < grEat; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 2;
+        }
+    }
+    for (let i = 0; i < pred; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 3;
+        }
+    }
+    for (let i = 0; i < flo; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 4;
+        }
+    }
+    for (let i = 0; i < regen; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 5;
+        }
+    }
+    return matrix;
+}
+
+let side = 20;
+
+let matrix = generator(25, 10, 10, 10, 8, 8);
+
+var grassArr = [];
+var grassEaterArr = []
+var predatorArr = []
+var flowerArr = []
+var regeneratorArr = []
+
+
+
+function setup() {
+    frameRate(5);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#E0FFFF');
+
+    for (var y = 0; y < matrix.length; ++y) {
+        for (var x = 0; x < matrix[y].length; ++x) {
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y, 1);
+                grassArr.push(gr);
+            }
+            else if (matrix[y][x] == 2) {
+                var eat = new GrassEater(x, y)
+                grassEaterArr.push(eat)
+            }
+            else if (matrix[y][x] == 3) {
+                var pred = new Predator(x, y)
+                predatorArr.push(pred)
+            }
+            else if (matrix[y][x] == 4) {
+                var flo = new Flower(x, y)
+                flowerArr.push(flo)
+            }
+            else if (matrix[y][x] == 5) {
+                var toxFlo = new Regenerator(x, y)
+                regeneratorArr.push(toxFlo)
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
+function draw() {
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 3) {
+                fill("red");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 4) {
+                fill("pink");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 5) {
+                fill("#4169E1");
+                rect(x * side, y * side, side, side);
+            }
+        }
+    }
+
+    for (var i in grassArr) {
+        grassArr[i].mul();
+    }
+
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].mul();
+        grassEaterArr[i].eat()
+    }
+
+    for (var i in predatorArr) {
+        predatorArr[i].mul();
+        predatorArr[i].eat()
+    }
+    for (let j in regeneratorArr) {
+        regeneratorArr[j].mul()
+    }
+}
